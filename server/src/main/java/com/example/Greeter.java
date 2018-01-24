72922984a31e5097866ee20a66e1606acc9177e7
wolf637 <alexey.volkov@live.com>
2018-01-23 21:44:49 -0800

    more fix on checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,9 +12,9 @@ public class Greeter {
 
   }
   /**
-	  * @param someone th name of a person
-	  * @return greeting string
-	  */
+  * @param someone th name of a person
+  * @return greeting string
+  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
